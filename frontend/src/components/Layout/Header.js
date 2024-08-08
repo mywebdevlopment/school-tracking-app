6753fca44b9f5c70989d45dc97d90146830e0f80
PavaniSurisetty <pavsurisetty@gmail.com>
2024-08-08 13:35:52 +0530

    frontend changes

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <nav className="bg-blue-500 p-4">
-      <ul className="flex space-x-4">
+      <ul className="flex space-x-8">
         <li><Link to="/" className="text-white">Home</Link></li>
         <li><Link to="/admin" className="text-white">Admin</Link></li>
         <li><Link to="/parent" className="text-white">Parent</Link></li>
