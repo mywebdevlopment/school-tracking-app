9634b859e32d264863e1d0b33b966adfcde9e5be
PavaniSurisetty <pavsurisetty@gmail.com>
2024-08-08 10:48:35 +0530

    backend  controller changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = process.env.MONGO_URI;
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
