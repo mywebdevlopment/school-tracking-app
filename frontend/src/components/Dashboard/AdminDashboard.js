de6141a34f5efc0334c225dda718432fd6fcc4e7
PavaniSurisetty <pavsurisetty@gmail.com>
2024-08-23 17:50:42 +0530

    frontend changes

--- frontend/src/components/Dashboard/AdminDashboard.js
+++ frontend/src/components/Dashboard/AdminDashboard.js
@@ -1,12 +1,13 @@
 // src/components/Dashboard/AdminDashboard.js
 import React from 'react';
-import ChildDetails from './Tracking/ChildDetails';
-import ChildList from './Tracking/ChildList'
+import ChildDetails from '../Tracking/ChildDetails';
+import ChildList from '../Tracking/ChildList'
 function AdminDashboard() {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold">Admin Dashboard</h1>.
       <ChildDetails />
+      <ChildList />
       {/* Add admin dashboard functionality here */}
     </div>
   );
