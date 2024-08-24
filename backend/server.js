298bb7342746a8dc53e75cf3cd0121dd3e6232c5
PavaniSurisetty <pavsurisetty@gmail.com>
2024-08-24 11:08:40 +0530

    changes server.js and backend

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,9 @@ connectDB();
 
 // Init Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000'  // or wherever your frontend is served from
+}));
 const mongoose = require('mongoose');
 
 const mongoURI = 'mongodb://localhost:27017/school-tracking-app';
@@ -28,7 +30,7 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/children', require('./routes/childRoutes'));
 app.use('/api/notifications', require('./routes/notificationRoutes'));
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Server is running');
