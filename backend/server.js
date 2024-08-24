ae646ebb59560f5f288a2060357e4b054bfed6cd
PavaniSurisetty <pavsurisetty@gmail.com>
2024-08-24 10:23:30 +0530

    changes server and register backend

--- backend/server.js
+++ backend/server.js
@@ -22,13 +22,6 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => console.log('MongoDB connection error:', err));
 require('dotenv').config();
-const mongoose = require('mongoose');
-
-const mongoURI = process.env.MONGO_URI;
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected successfully'))
-  .catch(err => console.log('MongoDB connection error:', err));
 
 // Define Routes
 app.use('/api/auth', require('./routes/authRoutes'));
