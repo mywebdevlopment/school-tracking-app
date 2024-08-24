8e2b8b254a68d7bb43beea79700d9ba0f6e4a283
PavaniSurisetty <pavsurisetty@gmail.com>
2024-08-24 11:05:58 +0530

    changes server.js and .env frontend

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/children', require('./routes/childRoutes'));
 app.use('/api/notifications', require('./routes/notificationRoutes'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.get('/', (req, res) => {
   res.send('Server is running');
