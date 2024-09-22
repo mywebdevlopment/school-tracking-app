e104e608a9b90e05b850b086ec9b2033e621f95c
Pavani Surisetty <137615690+PavaniSurisetty@users.noreply.github.com>
2024-09-22 20:24:05 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <div className="p-4">
-      <h1 className="text-3xl font-bold">Welcome to the School Tracking App</h1>
+      <h1 className="text-3xl font-bold">Welcome to the School Tracking App
+</h1>
       <p className="mt-4">Navigate to <Link to="/login" className="text-blue-500">login</Link> or <Link to="/register" className="text-blue-500">register</Link></p>
       <p className="mt-4">Navigate to <Link to="/admin" className="text-blue-500">Admin Dashboard</Link> or <Link to="/parent" className="text-blue-500">Parent Dashboard</Link></p>
     </div>
